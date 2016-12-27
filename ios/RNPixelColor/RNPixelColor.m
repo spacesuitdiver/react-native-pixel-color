cce96b23574da19b15f2d0ed026a3d77ea793f3f
Chris LeBlanc <chris@leblaaanc.com>
2016-12-27 15:40:23 -0600

    Fixes pixel color being completely wrong.

--- ios/RNPixelColor/RNPixelColor.m
+++ ios/RNPixelColor/RNPixelColor.m
@@ -11,8 +11,7 @@ RCT_EXPORT_METHOD(getHex:(NSString *)path
                   coords:(NSDictionary *)coords
                   callback:(RCTResponseSenderBlock)callback)
 {
-    NSInteger *x = [RCTConvert NSInteger:coords[@"x"]];
-    NSInteger *y= [RCTConvert NSInteger:coords[@"y"]];
+    CGPoint point = CGPointMake([RCTConvert NSInteger:coords[@"x"]], [RCTConvert NSInteger:coords[@"y"]]);
 
     [_bridge.imageLoader loadImageWithURLRequest:[RCTConvert NSURLRequest:path] callback:^(NSError *error, UIImage *image) {
         if (error || image == nil) { // if couldn't load from bridge create a new UIImage
@@ -29,28 +28,52 @@ RCT_EXPORT_METHOD(getHex:(NSString *)path
             }
         }
         
-        UIColor *color = pixelColorInImage(image, x, y);
+        UIColor *color = pixelColorInImage(image, point);
         callback(@[[NSNull null], hexStringForColor(color)]);
      
     }];
 }
 
-UIColor * pixelColorInImage( UIImage* image, int x, int y ) {
-    CFDataRef pixelData = CGDataProviderCopyData(CGImageGetDataProvider(image.CGImage));
-    const UInt8* data = CFDataGetBytePtr(pixelData);
+UIColor * pixelColorInImage( UIImage* image, CGPoint point) {
+    // Cancel if point is outside image coordinates
+    if (!CGRectContainsPoint(CGRectMake(0.0f, 0.0f, image.size.width, image.size.height), point)) {
+        return nil;
+    }
     
-    int pixelInfo = ((image.size.width * y) + x ) * 4; // 4 bytes per pixel
     
-    UInt8 red   = data[pixelInfo + 0];
-    UInt8 green = data[pixelInfo + 1];
-    UInt8 blue  = data[pixelInfo + 2];
-    UInt8 alpha = data[pixelInfo + 3];
-    CFRelease(pixelData);
+    // Create a 1x1 pixel byte array and bitmap context to draw the pixel into.
+    // Reference: http://stackoverflow.com/questions/1042830/retrieving-a-pixel-alpha-value-for-a-uiimage
+    NSInteger pointX = trunc(point.x);
+    NSInteger pointY = trunc(point.y);
+    CGImageRef cgImage = image.CGImage;
+    NSUInteger width = CGImageGetWidth(cgImage);
+    NSUInteger height = CGImageGetHeight(cgImage);
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    int bytesPerPixel = 4;
+    int bytesPerRow = bytesPerPixel * 1;
+    NSUInteger bitsPerComponent = 8;
+    unsigned char pixelData[4] = { 0, 0, 0, 0 };
+    CGContextRef context = CGBitmapContextCreate(pixelData,
+                                                 1,
+                                                 1,
+                                                 bitsPerComponent,
+                                                 bytesPerRow,
+                                                 colorSpace,
+                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGColorSpaceRelease(colorSpace);
+    CGContextSetBlendMode(context, kCGBlendModeCopy);
     
-    return [UIColor colorWithRed:red  /255.0f
-                           green:green/255.0f
-                            blue:blue /255.0f
-                           alpha:alpha/255.0f];
+    // Draw the pixel we are interested in onto the bitmap context
+    CGContextTranslateCTM(context, -pointX, -pointY);
+    CGContextDrawImage(context, CGRectMake(0.0f, 0.0f, (CGFloat)width, (CGFloat)height), cgImage);
+    CGContextRelease(context);
+    
+    // Convert color values [0..255] to floats [0.0..1.0]
+    CGFloat red   = (CGFloat)pixelData[0] / 255.0f;
+    CGFloat green = (CGFloat)pixelData[1] / 255.0f;
+    CGFloat blue  = (CGFloat)pixelData[2] / 255.0f;
+    CGFloat alpha = (CGFloat)pixelData[3] / 255.0f;
+    return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
 }
 
 NSString * hexStringForColor( UIColor* color ) {
