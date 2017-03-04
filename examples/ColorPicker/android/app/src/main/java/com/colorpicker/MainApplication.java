1836bdc336612800364d6f752cf18eaedcc014fe
Rodrigo Crespo <rcrespocepeda@gmail.com>
2017-03-04 21:24:22 +0100

    correct package

--- examples/ColorPicker/android/app/src/main/java/com/colorpicker/MainApplication.java
+++ examples/ColorPicker/android/app/src/main/java/com/colorpicker/MainApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import fr.bamlab.rnimageresizer.ImageResizerPackage;
+import eu.crowtec.rnpixelcolor.RNPixelColorPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ImageResizerPackage()
+            new RNPixelColorPackage()
       );
     }
   };
