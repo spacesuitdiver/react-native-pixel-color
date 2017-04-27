d67fe13ca104c1ad6fa7866b98385a3828301580
Vattanan Buranapugdee <vattanan.bu@gmail.com>
2017-04-27 23:11:51 +0700

    Split function create images and function get hex in file index.js

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ import {
 export default {
   getHex: (path, options) => new Promise((resolve, reject) => {
     NativeModules.RNPixelColor.getHex(path, options, (err, color) => {
+  createImage: (path, originalRotation) => new Promise((resolve, reject)=>{ 
+    NativeModules.RNPixelColor.createImage(path, Number(originalRotation), (err, res) => {
+      if (err) return reject(err);
+
+      resolve(res);
+    });
+  }),
       if (err) return reject(err);
 
       resolve(color);
