a5e1707a033b26ee837dee6c574d43dc2e6000c6
Jesús Darío <jsdario@users.noreply.github.com>
2017-10-02 17:06:16 +0200

    Update ImageResizerPackage.java

--- android/src/main/java/fr/bamlab/rnimageresizer/ImageResizerPackage.java
+++ android/src/main/java/fr/bamlab/rnimageresizer/ImageResizerPackage.java
@@ -33,7 +33,7 @@ public class ImageResizerPackage implements ReactPackage {
      * listed here. Also listing a native module here doesn't imply that the JS implementation of it
      * will be automatically included in the JS bundle.
      */
-    @Override
+    // Deprecated @Override for react-native >= 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
